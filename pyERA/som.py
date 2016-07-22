9ffd1e6b4fd793a69037ee58d4a7d0a8bd1b1661
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-07-22 12:41:35 +0100

    Comments to the function added

--- pyERA/som.py
+++ pyERA/som.py
@@ -29,31 +29,45 @@ class Som:
             self._weights_matrix = np.rint(self._weights_matrix)
 
     def return_weights_matrix(self):
+        """Get the weights matrix of the SOM.
+
+        """
         return self._weights_matrix
 
     def get_unit_weights(self, row, col):
-        """Return the weights associated with a given unit.
+        """Get the weights associated with a given unit.
 
         """
         return self._weights_matrix[row, col, :]
 
 
     def set_unit_weights(self, weights_vector, row, col):
+        """Set the weights associated with a given unit.
+
+        """
         self._weights_matrix[row, col, :] = weights_vector
 
 
-    def return_unit_square_neighborhood(self, row, col, ray):
+    def return_unit_square_neighborhood(self, row, col, radius):
+        """Return a list with (row, col, distance) of the units around a unit.
+
+        This version uses a square as radius, all the element inside the radius
+        are taken as neighborood.
+        @param row index of the unit
+        @param col the column index of the unit
+        @param radius the radius of the distance to consider
+        """
          output_list = list()
-         if(ray <= 0): output_list.append((row, col, 0)); return output_list #return empty if ray=0
-         ray = int(ray) #Precaution against float ray
-         row_range_min = row - ray
+         if(radius <= 0): output_list.append((row, col, 0)); return output_list #return empty if radius=0
+         radius = int(radius) #Precaution against float radius
+         row_range_min = row - radius
          if(row_range_min < 0): row_range_min = 0
-         row_range_max = row + ray
+         row_range_max = row + radius
          if(row_range_max >= self._matrix_size): row_range_max = self._matrix_size - 1
 
-         col_range_min = col - ray
+         col_range_min = col - radius
          if(col_range_min < 0): col_range_min = 0
-         col_range_max = col + ray
+         col_range_max = col + radius
          if(col_range_max >= self._matrix_size): col_range_max = self._matrix_size - 1
 
          for row_iter in range(row_range_min, row_range_max+1):
@@ -68,19 +82,27 @@ class Som:
 
          return output_list
 
-    def return_unit_round_neighborhood(self, row, col, ray):
+    def return_unit_round_neighborhood(self, row, col, radius):
+        """Return a list with (row, col, distance) of the units around a unit.
+
+        This version uses a circle as radius, all the element inside the radius
+        are taken as neighborood.
+        @param row index of the unit
+        @param col the column index of the unit
+        @param radius the radius of the distance to consider
+        """
          output_list = list()
-         if(ray <= 0): output_list.append((row, col, 0)); return output_list #return empty if ray=0
+         if(radius <= 0): output_list.append((row, col, 0)); return output_list #return empty if radius=0
 
          #Finding the square around the unit
-         #with wide=ray using the ceil of ray
-         row_range_min = row - int(np.ceil(ray))
+         #with wide=radius using the ceil of radius
+         row_range_min = row - int(np.ceil(radius))
          if(row_range_min < 0): row_range_min = 0
-         row_range_max = row + int(np.ceil(ray))
+         row_range_max = row + int(np.ceil(radius))
          if(row_range_max >= self._matrix_size): row_range_max = self._matrix_size - 1
-         col_range_min = col - int(np.ceil(ray))
+         col_range_min = col - int(np.ceil(radius))
          if(col_range_min < 0): col_range_min = 0
-         col_range_max = col + int(np.ceil(ray))
+         col_range_max = col + int(np.ceil(radius))
          if(col_range_max >= self._matrix_size): col_range_max = self._matrix_size - 1
 
          for row_iter in range(row_range_min, row_range_max+1):
@@ -91,16 +113,23 @@ class Som:
                  #Pitagora's Theorem to estimate distance
                  distance = np.sqrt( np.power(col_distance,2) + np.power(row_distance,2) )
                  #Store the unit only if the distance is
-                 #less than the ray
-                 if(distance <= ray): output_list.append((row_iter, col_iter, distance))
+                 #less than the radius
+                 if(distance <= radius): output_list.append((row_iter, col_iter, distance))
 
          return output_list
 
     def return_euclidean_distance(self, a, b):
+        """Return the Euclidean Distance between two numpy vectors.
+
+        """
         return np.linalg.norm(a-b)
 
 
     def return_BMU_index(self, input_vector):
+        """Return the coordinates of the BMU.
+
+        @param input_vector the vector to use for the comparison.
+        """
         output_matrix = np.zeros((self._matrix_size, self._matrix_size))
         it = np.nditer(output_matrix, flags=['multi_index'])
         while not it.finished:
@@ -113,6 +142,10 @@ class Som:
 
 
     def return_BMU_weights(self, input_vector):
+        """Return the weights of the BMU.
+
+        @param input_vector the vector to use for the comparison.
+        """
         output_matrix = np.zeros((self._matrix_size, self._matrix_size))
         it = np.nditer(output_matrix, flags=['multi_index'])
         while not it.finished:
@@ -123,7 +156,16 @@ class Som:
         row, col = np.unravel_index(output_matrix.argmin(), output_matrix.shape)
         return self._weights_matrix[row, col, :]
 
-    def training_single_step(self, input_vector, units_list, learning_rate, ray):
+
+    def training_single_step(self, input_vector, units_list, learning_rate, radius):
+        """A single step of the training procedure.
+
+        It updates the weights using the Kohoen learning rule.
+        @param input_vector the vector to use for the comparison.
+        @param units_list the units to modify (the BMU and neighborhood)
+        @param learning_rate
+        @param radius it is used to update the weights based on distance.
+        """
         for unit in units_list:
             row = unit[0]
             col = unit[1]
@@ -132,7 +174,7 @@ class Som:
             #The distance_rate take into account the distance of the unit
             #from the BMU and permits regulating the updating of the weights
             #with more decision for units that are close to the BMU.
-            #distance_rate = np.exp(- np.power(dist,2)/(2*np.power(ray,2)))
+            #distance_rate = np.exp(- np.power(dist,2)/(2*np.power(radius,2)))
 
             #Update the weights of the neighborood units
             #The new weight is equal to the old one plus a fracion
