79fdb09f60c6a9a465e75cfee14a680f4804afd0
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2018-04-19 13:42:22 +0100

    Adding the radius and linear distanc eupdate in training

--- pyERA/tfsom.py
+++ pyERA/tfsom.py
@@ -41,28 +41,58 @@ class tfSom:
         @param high boundary for the random initialization
         @param verbose
         """
-        #Global variables
+        #-Global variables
         self.tot_rows = tot_rows
         self.tot_cols = tot_cols
         self.depth = depth
-        #Placeholders
+        #-Placeholders
         self.input_placeholder = tf.placeholder(tf.float32, shape=depth, name="input_placeholder")
         self.learning_rate_placeholder = tf.placeholder(tf.float32, shape=None, name="learning_rate_placeholder")
-        #Variables and saver
+        self.radius_placeholder = tf.placeholder(tf.float32, shape=None, name="radius_placeholder")
+        #-Constants
+        indices_matrix = np.zeros((tot_rows, tot_cols, 2))
+        for row in range(tot_rows):
+            for col in range(tot_cols):
+                indices_matrix[row,col,:] = (row, col)
+        #print indices_matrix
+        #print "Check...."
+        #a = np.array([1,2])
+        #print(np.linalg.norm(a-indices_matrix, axis=2))
+        #return
+        grid_matrix = tf.constant(indices_matrix, shape=[tot_rows, tot_cols, depth])
+
+        #-Variables and saver
         self.weight = tf.get_variable("weights", [tot_rows, tot_cols, depth], initializer=tf.random_uniform_initializer(minval=low, maxval=high))
         self.tf_saver = tf.train.Saver({"weights": self.weight})
         #initializer=tf.random_normal_initializer(mean=0.0, stddev=1.0))
-        #Distance estimation
+        #-Distance estimation
         difference = tf.subtract(self.input_placeholder, self.weight)
         self.distance_matrix = tf.norm(difference, ord='euclidean', axis=2, name="euclidean_matrix")
-        #Train operations
-        delta = tf.multiply(self.learning_rate_placeholder, difference)
-        self.train = self.weight.assign(tf.add(self.weight, delta))
-        #Additional calls
+
+        #-Train operations
+        #find the index of the best matching unit
         distance_matrix_flatten = tf.reshape(self.distance_matrix, [-1])
         self.distance_argmin = tf.argmin(distance_matrix_flatten)
         self.distance_min = tf.gather(distance_matrix_flatten, self.distance_argmin)
-        #Error measures
+        #generate a tensor with the best matching unit coord
+        grid_matrix_flatten = tf.reshape(grid_matrix, [-1,2])
+        bmu_coord = tf.gather(grid_matrix_flatten, self.distance_argmin, axis=0)
+        #difference between the coord tensor and the constant grid matrix
+        difference_neighborhood = tf.subtract(bmu_coord, grid_matrix)
+        self.neighborhood_matrix = tf.norm(difference_neighborhood, ord='euclidean', axis=2, name="neighborhood_matrix")
+        #pass the neighborhood_matrix through a linear function
+        #after this step the BMU having distance 0 has distance 1
+        coefficient = tf.divide(-1.0, self.radius_placeholder)
+        self.neighborhood_matrix = tf.add(tf.multiply(coefficient, tf.cast(self.neighborhood_matrix, tf.float32)), 1.0) #linearly scale the distance
+        self.neighborhood_matrix = tf.clip_by_value(self.neighborhood_matrix, clip_value_min=0.0, clip_value_max=1.0)
+
+        #-Evaluate the delta
+        self.weighted_learning_rate_matrix = tf.multiply(self.learning_rate_placeholder, self.neighborhood_matrix)
+        self.weighted_learning_rate_matrix = tf.expand_dims(self.weighted_learning_rate_matrix, axis=2)
+        delta = tf.multiply(self.weighted_learning_rate_matrix, difference)
+        self.train = self.weight.assign(tf.add(self.weight, delta))
+
+        #-Error measures
         self.distance_mean = tf.reduce_mean(self.distance_matrix)
         weight_flatten = tf.reshape(self.weight, [-1, depth])
         self.bmu_array = tf.gather(weight_flatten, self.distance_argmin)
@@ -90,19 +120,21 @@ class tfSom:
         col = index - (row*self.tot_cols)
         return index, (row,col)
 
-    def training_single_step(self, sess, input_array, learning_rate, radius=None):
+    def training_single_step(self, sess, input_array, learning_rate, radius):
         """A single step of the training procedure.
 
         It updates the weights using the Kohoen learning rule.
         @param sess the tensorflow session
         @param input_array the vector to use for the comparison.
         @param learning_rate
-        @param radius (optional) positive real, used to update the weights based on distance.
+        @param radius positive real, used to update the weights based on distance.
+            all the weights < radius are updated
         @return the average distance of the weights from the input array, the min distance
         """
         output = sess.run([self.train, self.distance_mean, self.distance_min], 
                            feed_dict={self.input_placeholder: input_array, 
-                                      self.learning_rate_placeholder: learning_rate})
+                                      self.learning_rate_placeholder: learning_rate,
+                                      self.radius_placeholder: radius})
         return output[1], output[2]
         
 
@@ -154,8 +186,18 @@ def main():
         print("Weights: ")
         print(sess.run(my_som.weight))
         print("")
-        print("Distance: ")
+        print("Distance matrix: ")
         print(sess.run(my_som.distance_matrix, feed_dict={"input_placeholder:0": input_array}))
+        print("")
+        print("Learning rate matrix: ")
+        print(sess.run(my_som.weighted_learning_rate_matrix, feed_dict={"input_placeholder:0": input_array,
+                                                                        "learning_rate_placeholder:0": 0.1,
+                                                                        "radius_placeholder:0": 2.0}))
+        print("")
+        print("Neighborhood matrix: ")
+        print(sess.run(my_som.neighborhood_matrix, feed_dict={"input_placeholder:0": input_array,
+                                                              "learning_rate_placeholder:0": 0.1,
+                                                              "radius_placeholder:0": 2.0}))
         print("")
         print("Distance mean: ")
         print(sess.run(my_som.distance_mean, feed_dict={"input_placeholder:0": input_array}))
@@ -171,6 +213,11 @@ def main():
         print("BMU array: ")
         print(sess.run(my_som.bmu_array, feed_dict={"input_placeholder:0": input_array}))
         print("")
+        print("update...")
+        my_som.training_single_step(sess, input_array, learning_rate=0.1, radius=2.0)
+        print("Weights updated: ")
+        print(sess.run(my_som.weight))
+        print("")
         #print("Reconstruction error: ")
         #print(sess.run(my_som.reconstruction_error, feed_dict={"input_placeholder:0": input_array}))
         #print("")
@@ -181,7 +228,7 @@ def main():
     input_array = np.random.uniform(size=input_shape)
     for i in range(10):
         #input_array = np.random.uniform(size=input_shape)
-        average_distance, min_distance = my_som.training_single_step(sess, input_array, learning_rate=0.1)
+        average_distance, min_distance = my_som.training_single_step(sess, input_array, learning_rate=0.1, radius=2.0)
         print("Step number: " + str(i))
         print("Average distance: " + str(average_distance))
         print("Minimum distance: " + str(min_distance))
